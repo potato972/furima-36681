8b4bce4fce6c83ee0dbf22bcd42100dd2e8a835c
potato972 <mogmog.mog880088@gmail.com>
2021-12-28 00:36:13 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,8 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user)
 end
