f3aabaea2ad263e0baf1c4ee55f3a3c80f75c7a0
potato972 <mogmog.mog880088@gmail.com>
2022-01-02 17:23:52 +0900

    ActiveHash導入、バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
 
   def index
+   # @category = Item.order("created_at DESC")
   end
 
   private
 
   def item_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :Postage
+  belongs_to :area
+  belongs_to :day
+
+  validates :title, :items, presence: true
+  validates :category_id, :status_id, :postage_id, :area_id, :day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_name, presence: true
+  validates :content, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :postage_id, presence: true
+  validates :area_id, presence: true
+  validates :day_id, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\Z/ } do
+    validates :price, numericality: {only_integer: true, greater_then_or_equal_to: 300, less_then_or_equel_to: 9_999_999 },
+                      presence: { message: "can't be blank"}
+
   belongs_to :user
   has_one_attached :image
 end
