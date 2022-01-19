b99bbdc7bf57d6d8537a6588f40582f5871ce9ec
potato972 <mogmog.mog880088@gmail.com>
2022-01-19 20:16:22 +0900

    商品一覧機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-   # @category = Item.order("created_at DESC")
+   @items = Item.all
+   @items = Item.all.order("created_at DESC")
+
   end
   
   def new
